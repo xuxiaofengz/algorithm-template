3803793548dfe8831e71043f186b001e3b017ffc
xuxiaofengz <2025324285@qq.com>
2025-03-11 11:23:57 +0000

    Create 单调队列区间最值.cpp

---
+++ 数据结构模版/单调队列/单调队列区间最值.cpp
@@ -0,0 +1,17 @@
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> que;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            while(!que.empty()&&nums[que.front()]<=nums[i]){
+                que.pop_front();
+            }
+            que.push_front(i);
+            if(que.back()<=i-k){
+                que.pop_back();
+            }
+            if(i>=k-1){
+                res.push_back(nums[que.back()]);
+            }
+        }
+        return res;
+    }
