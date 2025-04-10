c02dce8885de1caa923d806ba2a347f790c5c01f
xuxiaofengz <2025324285@qq.com>
2025-04-10 21:03:46 +0800

    Create 区间修改-单点查询.cpp

---
+++ 数据结构模版/树状数组/区间修改-单点查询.cpp
@@ -0,0 +1,27 @@
+//洛谷：P3368 【模板】树状数组 2
+//区间修改，单点查询
+//依赖于查分思想
+
+int a[N];
+int tr[N];
+
+//区间修改[l,r]:add(l,val),add(r+1,-val)
+void add(int ind,int val){
+	while(ind<=n){
+		tr[ind]+=val;
+		ind+=ind&(-ind);
+	}
+}
+
+//单点查询：a[i]+sum(i)
+int sum(int ind){
+	int res=0;
+	while(ind){
+		res+=tr[ind];
+		ind-=ind&(-ind);
+	}
+	return res;
+}
+int sum(int l,int r){
+	return sum(r)-sum(l-1);
+}
