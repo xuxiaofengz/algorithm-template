e318088bf7b0cb26a051ece54d0578de599388ef
xuxiaofengz <2025324285@qq.com>
2025-04-11 16:44:10 +0800

    Update 线性推逆元及快速幂.cpp

--- 数据结构模版/数学/逆元/线性推逆元及快速幂.cpp
+++ 数据结构模版/数学/逆元/线性推逆元及快速幂.cpp
@@ -1,15 +1,15 @@
 //洛谷：P3811 【模板】模意义下的乘法逆元
 
 ll inv[N];
-ll n,p;
+ll n,mod;
 
 //费马小定理
 ll qpow(ll x,ll b)
 {
 	ll res=1;
 	while(b){
-		if(b&1)res=(res*x)%p;
-		x=(x*x)%p;
+		if(b&1)res=(res*x)%mod;
+		x=(x*x)%mod;
 		b>>=1;
 	}
 	return res;
@@ -19,12 +19,12 @@ int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);cout.tie(nullptr);
-	cin>>n>>p;
+	cin>>n>>mod;
 	inv[1]=1;
 	cout<<inv[1]<<"\n";
   //线性推
 	for(int i=2;i<=n;i++){
-		inv[i]=(p-p/i)*inv[p%i]%p;
+		inv[i]=(mod-mod/i)*inv[mod%i]%mod;
 		cout<<inv[i]<<"\n";
 	}
 	
