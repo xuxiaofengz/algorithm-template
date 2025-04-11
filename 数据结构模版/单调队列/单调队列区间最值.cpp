63257f3a0afe584b3da782afe25d9a91795c377a
xuxiaofengz <2025324285@qq.com>
2025-04-11 16:31:26 +0800

    Update 单调队列区间最值.cpp

--- 数据结构模版/单调队列/单调队列区间最值.cpp
+++ 数据结构模版/单调队列/单调队列区间最值.cpp
@@ -1,3 +1,4 @@
+//力扣：滑动窗口的最值
 vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> que;
         vector<int> res;
@@ -9,7 +10,7 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
             if(que.back()<=i-k){
                 que.pop_back();
             }
-            if(i>=k-1){
+            if(i>=k-1){//窗口足够大
                 res.push_back(nums[que.back()]);
             }
         }
